13a703539f72da725f0577b1ee5875e7ad713d25
Web_page <abdulnabi9257@gmail.com>
2025-01-29 22:45:08 +0530

    vercel13

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -132,5 +132,5 @@ MEDIA_URL = "users/images"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'users/static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'users/static')
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
